739123a0daa9e7a1328e4b437ae73fe665737a78
Nick <shuminskiy23@gmail.com>
2023-11-27 23:01:50 +0300

    work on ordersS

--- src/screen/Main/Orders/OrdersS.tsx
+++ src/screen/Main/Orders/OrdersS.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {BaseWrapperComponent} from "../../../components/baseWrapperComponent";
-import {Box, Text} from "native-base";
+import {Box, Image, Text} from "native-base";
 import {observer} from "mobx-react-lite";
 import {CommonScreenPropsType} from "../../../api/type";
 import BurgerMenuBtn from "../../../components/BurgerMenu/BurgerMenuBtn";
@@ -9,12 +9,13 @@ import {StatusBar} from "expo-status-bar";
 import {useGoBack} from "../../../utils/hook/useGoBack";
 import {isFutureDate} from "../../../utils/commonUtils";
 import AuthStore from "../../../store/AuthStore/auth-store";
-import {StyleSheet} from "react-native";
+import {ImageBackground, Platform, StyleSheet} from "react-native";
 import FreezeModal from "./FreezeModal";
 import {routerConstants} from "../../../constants/routerConstants";
 import {useFocusEffect} from "@react-navigation/native";
 import OpenShiftModal from "../Shift/OpenShiftModal";
-import { useIsFocused } from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
+import backgroundOrderImg from '../../../assets/Images/orders/backgroundOrder.png'
 
 type OrdersSProps = CommonScreenPropsType & {}
 const OrdersS = observer(({navigation, route}: OrdersSProps) => {
@@ -29,7 +30,6 @@ const OrdersS = observer(({navigation, route}: OrdersSProps) => {
     useGoBack(goBackPress)
     useEffect(() => {
         if (isFocused) {
-            console.log(1)
             const isFreeze = isFutureDate(executorSettings?.executors?.datetime_freeze_until) ||
                 !executorSettings?.executors?.datetime_workshift_until ||
                 !(+executorSettings.executors.ready_for_orders >= 1)
@@ -46,13 +46,70 @@ const OrdersS = observer(({navigation, route}: OrdersSProps) => {
 
     return (
         <>
-            <BaseWrapperComponent isKeyboardAwareScrollView={true}>
-                <StatusBar backgroundColor={colors.white}/>
-                <Box paddingX={4}>
-                    <BurgerMenuBtn openingForced={isOpenMenu}/>
-                    <Text>Orders</Text>
+            <BaseWrapperComponent styleSafeArea={{
+                paddingTop: 0,
+                backgroundColor: colors.blue
+            }} isKeyboardAwareScrollView={true}>
+                <Box backgroundColor={colors.blue}>
+                    <Box style={{height: 220}} backgroundColor={colors.blue}>
+                        <ImageBackground alt={'mg-header'} resizeMode={'contain'} style={{
+                            height: 284,
+                        }} source={backgroundOrderImg}>
+                            <Box paddingX={4} style={{paddingTop: Platform.OS === 'ios' ? 0 : 40}}>
+                                <BurgerMenuBtn openingForced={isOpenMenu}/>
+                                <Box backgroundColor={colors.white} mt={3} alignItems={'center'} p={3} w={'50%'}
+                                     borderRadius={50} borderWidth={5} borderColor={'#E8F5FE'}>
+                                    <Text color={colors.blue} fontSize={20} fontFamily={'semiBold'}>The shift is
+                                        open</Text>
+                                </Box>
+                            </Box>
+                        </ImageBackground>
+
+                    </Box>
+                    <Box borderTopLeftRadius={16} p={4} borderTopRightRadius={16} backgroundColor={colors.white}>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                        <Text>12121212121212121</Text>
+                    </Box>
+
                 </Box>
-                <FreezeModal freezeDate={executorSettings?.executors?.datetime_freeze_until}
+                {/*  <FreezeModal freezeDate={executorSettings?.executors?.datetime_freeze_until}
                              onPress={() => {
                                  setIsFreeze(false)
                                  setIsOpenShiftModal(true)
@@ -61,7 +118,7 @@ const OrdersS = observer(({navigation, route}: OrdersSProps) => {
                 <OpenShiftModal visible={isOpenShiftModal} onClose={() => {
                     setIsFreeze(false)
                     setIsOpenShiftModal(false)
-                }}/>
+                }}/>*/}
             </BaseWrapperComponent>
         </>
     );
